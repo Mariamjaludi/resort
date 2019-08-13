5bdec5bcc560f86da80fb58fe8cb1ce31522710f
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-13 20:41:06 +0300

    banner added to rooms, error, and home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero'
-
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
  const Home = () => {
    return (
-     <Hero hero="defaultHero"/>
+     <Hero>
+       <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting at $299">
+         <Link to='/rooms' className="btn-primary">Our Rooms</Link>
+       </Banner>
+     </Hero>
    )
  }
 
